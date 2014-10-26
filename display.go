1f3bb7d94923ce9ff6883f6a2517736aa1fa0c78
Joseph Hager <ajhager@gmail.com>
2014-10-25 23:25:52 -0400

    Consolidate all display types

--- display.go
+++ display.go
@@ -135,10 +135,54 @@ func (d DisplayObjectContainer) RemoveAllChildren() {
 	d.Call("removeChildren")
 }
 
+type Sprite struct {
+	*DisplayObjectContainer
+	Anchor    Point
+	Tint      uint32 `js:"tint"`
+	BlendMode int    `js:"blendMode"`
+}
+
+func NewSprite(texture *Texture) *Sprite {
+	object := pkg.Get("Sprite").New(texture.Object)
+	return wrapSprite(object)
+}
+
+func wrapSprite(object js.Object) *Sprite {
+	return &Sprite{
+		DisplayObjectContainer: wrapDisplayObjectContainer(object),
+		Anchor:                 Point{Object: object.Get("anchor")},
+	}
+}
+
+// SetTexture sets the texture of the sprite.
+func (s *Sprite) SetTexture(texture *Texture) {
+	s.Call("setTexture", texture.Object)
+}
+
+func SpriteFromFrame(frameId string) *Sprite {
+	return wrapSprite(pkg.Get("Sprite").Call("fromFrame", frameId))
+}
+
+func SpriteFromImage(imageId string, crossOrigin bool, scaleMode int) *Sprite {
+	return wrapSprite(pkg.Get("Sprite").Call("fromImage", imageId, crossOrigin, scaleMode))
+}
+
 type SpriteBatch struct {
 	js.Object
 }
 
 func NewSpriteBatch() *SpriteBatch {
 	return &SpriteBatch{wrapDisplayObjectContainer(pkg.Get("SpriteBatch").New())}
+}
+
+type Stage struct {
+	*DisplayObjectContainer
+}
+
+func NewStage(background uint32) *Stage {
+	return wrapStage(pkg.Get("Stage").New(background))
+}
+
+func wrapStage(object js.Object) *Stage {
+	return &Stage{DisplayObjectContainer: wrapDisplayObjectContainer(object)}
 }
