7753eea5cf601974d02d5e701c35f0dd05057128
Archs <gitarchs@yeah.net>
2015-05-22 01:16:22 +0800

    animate every 2 frames

--- examples/9test/main.go
+++ examples/9test/main.go
@@ -14,27 +14,33 @@ var (
 	render   = pixi.AutoDetectRenderer(300, 300)
 	sprite   = pixi.SpriteFromImage("img/bunny.png", false, pixi.ScaleModes.Default)
 	lastTime = float64(0)
-	stepX    = float64(2)
-	stepY    = float64(2)
+	stepX    = float64(5)
+	stepY    = float64(3)
+	counter  = 0
 )
 
 func animate(t float64) {
-	if sprite.Position.X > 300 || sprite.Position.X < 0 {
+	defer raf.RequestAnimationFrame(animate)
+	counter += 1
+	if counter%2 != 0 {
+		return
+	}
+	// println(counter, sprite.Position.X, sprite.Position.Y)
+	if sprite.Position.X >= 290 || sprite.Position.X <= 10 {
 		stepX = -1 * stepX
 	}
-	if sprite.Position.Y > 300 || sprite.Position.Y < 0 {
+	if sprite.Position.Y >= 290 || sprite.Position.Y <= 10 {
 		stepY = -1 * stepY
 	}
-	sprite.Position.Set(sprite.Position.X+rand.Float64()+stepX,
-		sprite.Position.Y+rand.Float64()+stepY)
+	sprite.Position.Set(sprite.Position.X+rand.Float64()*stepX,
+		sprite.Position.Y+rand.Float64()*stepY)
 	sprite.Rotation = math.Mod(t/100, 2*math.Pi)
 	render.Render(stage)
-	raf.RequestAnimationFrame(animate)
 }
 
 func run() {
 	sprite.Anchor.SetTo(0.5)
-	sprite.Position.SetTo(0)
+	sprite.Position.SetTo(20)
 	stage.AddChild(sprite)
 	render.Render(stage)
 	el := dom.Wrap(render.View)
