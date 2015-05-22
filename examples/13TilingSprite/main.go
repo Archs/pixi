c1d64e7960edef97e62e7a17d3ac26d96e0e3daa
Archs <sun@archs.tk>
2015-05-22 14:26:32 +0800

    add examples 13TilingSprite

---
+++ examples/13TilingSprite/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/Archs/js/dom"
+	"github.com/Archs/js/raf"
+	"github.com/Archs/pixi"
+	"math"
+)
+
+var (
+	stage        = pixi.NewStage(0x1099bb)
+	renderer     = pixi.AutoDetectRenderer(800, 600)
+	texture      = pixi.TextureFromImage("img/p2.jpeg", false, pixi.ScaleModes.Default)
+	tilingSprite = pixi.NewTilingSprite(texture, renderer.Width, renderer.Height)
+
+	counter = 0.05
+)
+
+func run(t float64) {
+	counter += 0.05
+	raf.RequestAnimationFrame(run)
+	tilingSprite.TileScale.Set(math.Sin(counter)+2, math.Cos(counter)+2)
+	tilingSprite.TilePosition.SetTo(counter + 2)
+	renderer.Render(stage)
+}
+
+func main() {
+	stage.AddChild(tilingSprite)
+	dom.OnDOMContentLoaded(func() {
+		el := dom.Wrap(renderer.View)
+		dom.Body().AppendChild(el)
+		raf.RequestAnimationFrame(run)
+	})
+}
