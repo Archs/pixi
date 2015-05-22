fbc7d424dd7f965f090e2377de302b23ff8a58e6
Archs <gitarchs@yeah.net>
2015-05-22 16:45:56 +0800

    update README

---
+++ examples/91Test/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/Archs/js/dom"
+	"github.com/Archs/js/raf"
+	"gopkg.in/Archs/pixi.v3"
+)
+
+var (
+	stage    = pixi.NewContainer()
+	renderer = pixi.AutoDetectRenderer(800, 600, 0x00000)
+)
+
+func animate(t float64) {
+	raf.RequestAnimationFrame(animate)
+	renderer.Render(stage)
+}
+
+func main() {
+	dom.OnDOMContentLoaded(func() {
+		el := dom.Wrap(renderer.View)
+		dom.Body().AppendChild(el)
+		raf.RequestAnimationFrame(animate)
+	})
+}
