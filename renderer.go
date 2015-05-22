4d5f53da6465fa9c1ddcde9515ae7d8b18b02d0d
Archs <sun@archs.tk>
2015-05-22 14:09:20 +0800

    add Width/Height for pixi.Renderer

--- renderer.go
+++ renderer.go
@@ -5,6 +5,12 @@ import "github.com/gopherjs/gopherjs/js"
 type Renderer struct {
 	*js.Object
 	View *js.Object `js:"view"`
+	// 	width	number	800	optional
+	// the width of the canvas view
+	Width float64 `js:"width"`
+	// height	number	600	optional
+	// the height of the canvas view
+	Height float64 `js:"height"`
 }
 
 func (r Renderer) Render(stage *Stage) {
