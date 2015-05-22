d8ed9c94d6f12c5167c960119fc1e69dac35976f
Archs <gitarchs@yeah.net>
2015-05-22 09:16:50 +0800

    update

--- examples/10Particles/main.go
+++ examples/10Particles/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"container/list"
 	"github.com/Archs/js/dom"
 	"github.com/Archs/js/raf"
 	"github.com/Archs/pixi"
@@ -13,9 +12,9 @@ import (
 var (
 	stage    = pixi.NewStage(0x0)
 	renderer pixi.Renderer
-	ps       = list.New()
 	ctx      = pixi.NewGraphics()
 	COLOURS  = []float64{0x69D2E7, 0xA7DBD8, 0xE0E4CC, 0xF38630, 0xFA6900, 0xFF4E50, 0xF9D423}
+	mp       = make(map[int]*Particle)
 )
 
 type Particle struct {
@@ -27,50 +26,44 @@ type Particle struct {
 	force   float64
 	damping float64
 	color   float64
-	el      *list.Element
 }
 
 func newParticle(x, y float64) *Particle {
 	b := &Particle{
-		Graphics: pixi.NewGraphics(),
-		// Graphics: ctx,
-		x:       x,
-		y:       y,
-		radius:  5 + 30*rand.Float64(),
-		theta:   2 * math.Pi * rand.Float64(),
-		force:   2 + 8*rand.Float64(),
-		damping: 0.92,
-		color:   COLOURS[rand.Intn(7)],
+		// Graphics: pixi.NewGraphics(),
+		Graphics: ctx,
+		x:        x,
+		y:        y,
+		radius:   5 + 30*rand.Float64(),
+		theta:    2 * math.Pi * rand.Float64(),
+		force:    2 + 8*rand.Float64(),
+		damping:  0.92,
+		color:    COLOURS[rand.Intn(7)],
 	}
 	b.vx = b.force * math.Sin(b.theta)
 	b.vy = b.force * math.Cos(b.theta)
-	b.el = ps.PushBack(b)
 	return b
 }
 
+func (p *Particle) isAlive() bool {
+	return p.radius > 0.5
+}
+
 func (g *Particle) draw(t float64) {
-	g.Clear()
+	// g.Clear()
 	// update
 	g.radius *= 0.9
-	if g.radius < 1 {
-		g.remove()
-		return
-	}
 	g.x += g.vx
 	g.y += g.vy
-	// g.theta = 2 * math.Pi * rand.Float64()
-	// g.vx *= g.damping
-	// g.vy *= g.damping
-	// g.vx += math.Sin(g.theta) * 0.1
-	// g.vy += math.Cos(g.theta) * 0.1
+	g.theta = 2 * math.Pi * rand.Float64()
+	g.vx *= g.damping
+	g.vy *= g.damping
+	g.vx += math.Sin(g.theta) * 0.1
+	g.vy += math.Cos(g.theta) * 0.1
 	// draw
 	g.BeginFill(g.color, 1)
 	g.DrawCircle(g.x, g.y, g.radius)
 	g.EndFill()
-}
-
-func (p *Particle) remove() {
-	ps.Remove(p.el)
 }
 
 func run(t float64) {
@@ -80,11 +73,15 @@ func run(t float64) {
 	if n%3 != 0 {
 		return
 	}
-	if ps.Len() > 0 {
-		// ctx.Clear()
-		for e := ps.Front(); e != nil; e = e.Next() {
-			p := e.Value.(*Particle)
+	if len(mp) > 0 {
+		ctx.Clear()
+		for k, p := range mp {
 			p.draw(t)
+			if !p.isAlive() {
+				delete(mp, k)
+				p.Clear()
+				stage.RemoveChild(p)
+			}
 		}
 	}
 	renderer.Render(stage)
@@ -93,6 +90,7 @@ func run(t float64) {
 func makeParticles(x, y float64, n int) {
 	for i := 0; i < rand.Intn(n); i++ {
 		p := newParticle(x, y)
+		mp[len(mp)] = p
 		stage.AddChild(p)
 	}
 }
