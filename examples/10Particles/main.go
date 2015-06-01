134eb1de92b77fd9334e8e97bf5a0cc483dfc7f9
Archs <gitarchs@yeah.net>
2015-06-01 13:52:19 +0800

    update

--- examples/10Particles/main.go
+++ examples/10Particles/main.go
@@ -1,48 +1,61 @@
 package main
 
 import (
+	"container/list"
 	"github.com/Archs/js/dom"
 	"github.com/Archs/js/raf"
 	"github.com/Archs/pixi"
-	"math"
 	// "math"
 	"math/rand"
 )
 
 var (
-	stage    = pixi.NewStage(0xf2f2f2)
+	stage    = pixi.NewStage(0x040404)
 	renderer pixi.Renderer
-	bs       = []*Ball{}
+	ps       = list.New()
+	COLOURS  = []float64{0x69D2E7, 0xA7DBD8, 0xE0E4CC, 0xF38630, 0xFA6900, 0xFF4E50, 0xF9D423}
 )
 
-type Ball struct {
+type Particle struct {
 	*pixi.Graphics
-	x, y float64
+	x, y   float64
+	radius float64
+	color  float64
+	el     *list.Element
 }
 
-func newBall() *Ball {
-	b := &Ball{
+func newParticle() *Particle {
+	b := &Particle{
 		Graphics: pixi.NewGraphics(),
 		x:        0,
 		y:        0,
+		radius:   50,
+		color:    COLOURS[rand.Intn(7)],
 	}
-	b.LineStyle(3, 0xff9387*rand.Float64(), 1)
-	bs = append(bs, b)
+	b.el = ps.PushBack(b)
 	return b
 }
 
-func (g *Ball) draw(t float64) {
+func (g *Particle) draw(t float64) {
 	g.Clear()
-	// g.BeginFill(0xFF3300*rand.Float64(), rand.Float64())
-	g.LineStyle(3, 0xff9387*rand.Float64(), 1)
-	// g.DrawCircle(50+rand.Float64()*20, 50+rand.Float64()*20, 40+rand.Float64()*5)
-	g.DrawCircle(g.x, g.y, math.Mod(t, 50))
-	// g.EndFill()
+	if g.radius < 0 {
+		return
+		g.remove()
+	}
+	g.radius -= rand.Float64()
+	g.BeginFill(g.color, 0.5)
+	g.DrawCircle(g.x, g.y, g.radius)
+	g.EndFill()
+}
+
+func (p *Particle) remove() {
+	ps.Remove(p.el)
 }
 
 func run(t float64) {
-	for _, b := range bs {
-		b.draw(t)
+	for e := ps.Front(); e != nil; e = e.Next() {
+		p := e.Value.(*Particle)
+		p.draw(t)
 	}
 	renderer.Render(stage)
 	raf.RequestAnimationFrame(run)
@@ -50,16 +63,12 @@ func run(t float64) {
 
 func main() {
 	stage.Click(func(id *pixi.InteractionData) {
-		println(id.Global.X, id.Global.Y)
-		b := newBall()
-		b.x = id.Global.X
-		b.y = id.Global.Y
-		stage.AddChild(b)
+		p := newParticle()
+		p.x = id.Global.X
+		p.y = id.Global.Y
+		stage.AddChild(p)
 	})
-	g := newBall()
-	stage.AddChild(g)
 	dom.OnDOMContentLoaded(func() {
-		println("dom loaded")
 		dom.Body().Style.SetProperty("margin", "0")
 		renderer = pixi.AutoDetectRenderer(dom.Window().InnerWidth, dom.Window().InnerHeight)
 		v := dom.Wrap(renderer.View)
