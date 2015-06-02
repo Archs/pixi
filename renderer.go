9cf114a8084066c31e715ff196c554cfd0a479b6
Archs <sun@archs.tk>
2015-06-03 00:00:48 +0800

    fullfill PIXI.Renderer

--- renderer.go
+++ renderer.go
@@ -11,12 +11,66 @@ type Renderer struct {
 	// height	number	600	optional
 	// the height of the canvas view
 	Height float64 `js:"height"`
+
+	// autoResize boolean
+	// Whether the render view should be resized automatically
+	AutoResize bool `js:"autoResize"`
+
+	// blendModes object.<string, mixed>
+	// Tracks the blend modes useful for this renderer.
+	BlendModes *js.Object `js:"blendModes"`
+
+	// clearBeforeRender boolean
+	// This sets if the CanvasRenderer will clear the canvas or not before the new render pass. If the scene is NOT transparent Pixi will use a canvas sized fillRect operation every frame to set the canvas background color. If the scene is transparent Pixi will use clearRect to clear the canvas every frame. Disable this by setting this to false. For example if your game has a canvas filling background image you often don't need this set.
+	ClearBeforeRender bool `js:"clearBeforeRender"`
+
+	// preserveDrawingBuffer boolean
+	// The value of the preserveDrawingBuffer flag affects whether or not the contents of the stencil buffer is retained after rendering.
+	PreserveDrawingBuffer bool `js:"preserveDrawingBuffer"`
+
+	// resolution number
+	// The resolution of the renderer
+	// Default Value:
+	// 1
+	Resolution int `js:"resolution"`
+
+	// transparent boolean
+	// Whether the render view is transparent
+	Transparent bool `js:"transparent"`
+
+	// type RENDERER_TYPE
+	// The returntype of the renderer.
+	// Default Value:
+	// CONT.RENDERER_TYPE.UNKNOWN
+	Type bool `js:"type"`
 }
 
 func (r Renderer) Render(c *Container) {
 	r.Call("render", c.Object)
 }
 
-func AutoDetectRenderer(width, height int) Renderer {
-	return Renderer{Object: pkg.Call("autoDetectRenderer", width, height)}
+// resize(width, height)
+//
+// Resizes the canvas view to the specified width and height
+//	 Name	Type	Description
+//	 width	number
+//	 the new width of the canvas view
+//	 height	number
+//	 the new height of the canvas view
+func (r *Renderer) Resize(width, height float64) {
+	r.Call("resize", width, height)
+}
+
+// destroy(removeView)
+//
+// Removes everything from the renderer and optionally removes the Canvas DOM element.
+//	 Name	Type	Default	Description
+//	 removeView	boolean	false	optional
+//	 Removes the Canvas element from the DOM.
+func (r *Renderer) Destroy(removeView bool) {
+	r.Call("destroy", removeView)
+}
+
+func AutoDetectRenderer(width, height int) *Renderer {
+	return &Renderer{Object: pkg.Call("autoDetectRenderer", width, height)}
 }
