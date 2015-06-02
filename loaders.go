f29d09acb6ab597df2947cd3cb41f9daa583f47c
Archs <gitarchs@yeah.net>
2015-06-02 23:21:47 +0800

    update loader to PIXI V3

--- loaders.go
+++ loaders.go
@@ -2,22 +2,38 @@ package pixi
 
 import "github.com/gopherjs/gopherjs/js"
 
-type AssetLoader struct {
+var (
+	Loader = NewResourceLoader()
+)
+
+type ResourceLoader struct {
 	*js.Object
 }
 
-func NewAssetLoader(urls []string, crossOrigin bool) *AssetLoader {
-	return &AssetLoader{Object: pkg.Get("AssetLoader").New(urls, crossOrigin)}
+func NewResourceLoader() *ResourceLoader {
+	return &ResourceLoader{Object: pkg.Get("loaders").Get("Loader").New()}
 }
 
-func (a *AssetLoader) OnComplete(cb func()) {
-	a.Set("onComplete", cb)
+// Adds a resource (or multiple resources) to the loader queue.
+func (r *ResourceLoader) Add(name, url string, onComplete ...func()) *ResourceLoader {
+	if len(onComplete) > 0 {
+		r.Call("add", name, url, onComplete[0])
+	} else {
+		r.Call("add", name, url)
+	}
+	return r
 }
 
-func (a *AssetLoader) OnProgress(cb func()) {
-	a.Set("onProgress", cb)
-}
-
-func (a *AssetLoader) Load() {
-	a.Call("load")
+// Starts loading the queued resources.
+// Optinal callback that will be bound to the `complete` event
+func (r *ResourceLoader) Load(callback ...func(resource *js.Object)) *ResourceLoader {
+	if len(callback) > 0 {
+		fn := func(loader, res *js.Object) {
+			callback[0](res)
+		}
+		r.Call("load", fn)
+	} else {
+		r.Call("load")
+	}
+	return r
 }
